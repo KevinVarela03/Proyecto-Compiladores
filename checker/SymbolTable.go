4282947bdf69d8dc1e94c6b330e919c60f77cc7b
noni <noni45100@hotmail.com>
2024-04-24 10:54:28 -0600

    propuesta SymbolTable

--- checker/SymbolTable.go
+++ checker/SymbolTable.go
@@ -1,73 +1,89 @@
 package checker
 
-// SymbolTable representa la tabla de símbolos del compilador MiniGo
-type SymbolTable struct {
-	Parent    *SymbolTable         // Ámbito padre
-	Variables map[string]Type      // Mapa de identificadores a tipos bote
-	Functions map[string]Function  // Mapa de funciones a sus definiciones
-	Structs   map[string]Structure // Mapa de estructuras a sus definiciones
+import (
+	"fmt"
+)
+
+type Ident struct {
+	tok   string
+	typ   int
+	nivel int
+	valor int
 }
 
-// Type representa un tipo de dato en MiniGo
-type Type string
-
-// Function representa la definición de una función en MiniGo
-type Function struct {
-	Name       string   // Nombre de la función
-	ReturnType Type     // Tipo de retorno de la función
-	Parameters []Symbol // Parámetros de la función
+type VarIdent struct {
+	Ident
+	isConstant bool
 }
 
-// Structure representa la definición de una estructura en MiniGo
-type Structure struct {
-	Name   string          // Nombre de la estructura
-	Fields map[string]Type // Mapa de campos a tipos en la estructura
+type MethodIdent struct {
+	Ident
+	params []int
 }
 
-// Symbol representa un identificador en MiniGo
-type Symbol struct {
-	Name string // Nombre del identificador
-	Type Type   // Tipo del identificador
+type TablaSimbolos struct {
+	tabla       []Ident
+	nivelActual int
 }
 
-// NewSymbolTable crea una nueva tabla de símbolos con un ámbito padre
-func NewSymbolTable(parent *SymbolTable) *SymbolTable {
-	return &SymbolTable{
-		Parent:    parent,
-		Variables: make(map[string]Type),
-		Functions: make(map[string]Function),
-		Structs:   make(map[string]Structure),
+func NewTablaSimbolos() *TablaSimbolos {
+	return &TablaSimbolos{
+		tabla:       make([]Ident, 0),
+		nivelActual: -1,
 	}
 }
 
-// AddVariable agrega un identificador de variable a la tabla de símbolos
-func (st *SymbolTable) AddVariable(name string, dataType Type) {
-	st.Variables[name] = dataType
+func (t *TablaSimbolos) Insertar(tok string, typ int, isConstant bool) {
+	i := VarIdent{Ident: Ident{tok: tok, typ: typ, nivel: t.nivelActual, valor: 0}, isConstant: isConstant}
+	t.tabla = append(t.tabla, i.Ident)
 }
 
-// AddFunction agrega una definición de función a la tabla de símbolos
-func (st *SymbolTable) AddFunction(name string, function Function) {
-	st.Functions[name] = function
+func (t *TablaSimbolos) InsertarFunc(tok string, typ int, params []int) {
+	i := MethodIdent{Ident: Ident{tok: tok, typ: typ, nivel: t.nivelActual, valor: 0}, params: params}
+	t.tabla = append(t.tabla, i.Ident)
 }
 
-// AddStructure agrega una definición de estructura a la tabla de símbolos
-func (st *SymbolTable) AddStructure(name string, structure Structure) {
-	st.Structs[name] = structure
-}
-
-// Lookup busca un identificador en la tabla de símbolos y sus ámbitos padres
-func (st *SymbolTable) Lookup(name string) (Type, bool) {
-	// Buscar en el ámbito actual
-	dataType, found := st.Variables[name]
-	if found {
-		return dataType, true
+func (t *TablaSimbolos) Buscar(nombre string) *Ident {
+	for _, id := range t.tabla {
+		if id.tok == nombre {
+			return &id
+		}
 	}
+	return nil
+}
 
-	// Buscar en ámbitos padres si existen
-	if st.Parent != nil {
-		return st.Parent.Lookup(name)
+func (t *TablaSimbolos) BuscarNivelActual(nombre string) *Ident {
+	var temp *Ident
+	tempNivel := t.nivelActual
+	for _, id := range t.tabla {
+		if tempNivel == id.nivel {
+			if id.tok == nombre {
+				temp = &id
+			}
+		} else {
+			break
+		}
 	}
+	return temp
+}
 
-	// Identificador no encontrado
-	return "", false
+func (t *TablaSimbolos) OpenScope() {
+	t.nivelActual++
+}
+
+func (t *TablaSimbolos) CloseScope() {
+	for i := len(t.tabla) - 1; i >= 0; i-- {
+		if t.tabla[i].nivel == t.nivelActual {
+			t.tabla = append(t.tabla[:i], t.tabla[i+1:]...)
+		}
+	}
+	t.nivelActual--
+}
+
+func (t *TablaSimbolos) Imprimir() {
+	fmt.Println("----- INICIO TABLA ------")
+	for _, s := range t.tabla {
+		fmt.Printf("Nombre: %s - %d - %d\n", s.tok, s.nivel, s.typ)
+	}
+	fmt.Println("----- FIN TABLA ------")
 }
