684b6972cd306d3ee0cea463dcd34c94ba83d4c7
noni <noni45100@hotmail.com>
2024-04-26 19:32:18 -0600

    Test4

---
+++ Proyecto.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"Proyecto_Compiladores/checker"
+	"Proyecto_Compiladores/parser"
+	"fmt"
+	"github.com/antlr4-go/antlr/v4"
+)
+
+type MyErrorListener struct {
+	*antlr.DefaultErrorListener
+}
+
+func NewMyErrorListener() *MyErrorListener {
+	return new(MyErrorListener)
+}
+
+func (d *MyErrorListener) SyntaxError(recognizer antlr.Recognizer, offendingSymbol interface{}, line, column int, msg string, e antlr.RecognitionException) {
+	fmt.Printf("Error de sintaxis en la línea %d, columna %d: %s\n", line, column, msg)
+}
+
+func main() {
+
+	// Crear un analizador léxico para el archivo de entrada
+	input, _ := antlr.NewFileStream("test.txt")
+	lexer := parser.NewMiniGoScanner(input)
+	lexer.RemoveErrorListeners()                 // Remover los listeners de errores por defecto
+	lexer.AddErrorListener(NewMyErrorListener()) // Añadir tu propio listener de errores
+
+	// Crear un flujo de tokens a partir del analizador léxico
+	stream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
+
+	// Crear un analizador sintáctico para el flujo de tokens
+	p := parser.NewMiniGoParser(stream)
+	p.RemoveErrorListeners()                 // Remover los listeners de errores por defecto
+	p.AddErrorListener(NewMyErrorListener()) // Añadir tu propio listener de errores
+
+	// Construir el AST utilizando el analizador sintáctico
+	tree := p.Root()
+
+	// Crear una tabla de símbolos global
+	globalSymbolTable := checker.NewSymbolTable()
+
+	// Crear un checker con la tabla de símbolos global
+	check := &checker.Checker{
+			SymbolTable: globalSymbolTable,
+	}
+	check.Visit(tree)
+}
---
+++ checker/ASTNode.go
@@ -0,0 +1,35 @@
+package checker
+
+// NodeType representa los tipos de nodos en el AST
+type NodeType int
+
+const (
+	// NodeTypeFunction representa un nodo de declaración de función
+	NodeTypeFunction NodeType = iota + 1
+	// NodeTypeVariableDeclaration representa un nodo de declaración de variable
+	NodeTypeVariableDeclaration
+	// NodeTypeAssignment representa un nodo de asignación de variable
+	NodeTypeAssignment
+	// NodeTypeIdentifier representa un nodo de identificador
+	NodeTypeIdentifier
+	// NodeTypeType representa un nodo de tipo de dato
+	NodeTypeType
+	// NodeTypeLiteral representa un nodo de literal
+	NodeTypeLiteral
+)
+
+// ASTNode representa un nodo en el árbol de sintaxis abstracta (AST)
+type ASTNode struct {
+	Type     NodeType    // Tipo de nodo
+	Value    string      // Valor del nodo (nombre de función, identificador, valor literal, etc.)
+	Children []*ASTNode  // Hijos del nodo (si los tiene)
+}
+
+// NewASTNode crea un nuevo nodo AST
+func NewASTNode(nodeType NodeType, value string, children ...*ASTNode) *ASTNode {
+	return &ASTNode{
+		Type:     nodeType,
+		Value:    value,
+		Children: children,
+	}
+}
